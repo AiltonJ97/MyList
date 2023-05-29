c72949f84133c9552482b6fea597b9d7bc86ba81
Ailton Junio <ailtonjunio97@gmail.com>
2023-05-29 19:23:28 -0300

    Criando listPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:lista_tarefas/list_page.dart';
+
+void main(){
+  runApp(Home());
+}
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return 
+    return MaterialApp(
+      home: ListPage(),
+    );
   }
 }
+
