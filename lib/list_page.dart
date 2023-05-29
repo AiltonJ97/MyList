ac71702bb2694af506ddc5b06bc746a6308e96fc
Ailton Junio <ailtonjunio97@gmail.com>
2023-05-29 19:55:00 -0300

    Conhecendo as ferramentas

--- lib/list_page.dart
+++ lib/list_page.dart
@@ -5,8 +5,34 @@ class ListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: TextField(
+            decoration: InputDecoration(
+              labelText: 'E-mail',
+              hintText: 'Exemplo@gmail.com',
+              //border: OutlineInputBorder(),
+              //errorText: 'Campo obrigatorio',
+              prefixText: 'R\$ ', //Inicio do texto fixo
+              suffixText: 'cm', suffixStyle: TextStyle(color: Colors.red), //final do texto fixo
+              labelStyle: TextStyle(
+                fontSize: 40,
+              ),
+            ),
+            style: TextStyle(
+              //fontSize: 40, //Tamanho da fonte
+              fontWeight: FontWeight.w400, //peso da fonte
+              color: Colors.purple,
+            ),
+            //keyboardType: TextInputType.number,
+            //keyboardType: TextInputType.emailAddress,
+            //obscureText: true, //Esconder texto
+            //obscuringCharacter: 'Y', // Modificar caractere
+          ),
+        ),
+      ),
     );
   }
 }
